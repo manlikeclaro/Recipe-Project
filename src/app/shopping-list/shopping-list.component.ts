c0fea9e04456c303b5ed8fc0dd677033267b91e5
manlikeclaro <ckibii08@gmail.com>
2024-07-18 17:29:28 +0300

    [feat] added html templates for all components

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Ingredient} from "../shared/ingredient.model";
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrl: './shopping-list.component.css'
 })
 export class ShoppingListComponent {
+  ingredients: Ingredient[] = [
+    new Ingredient("Flour", 250),
+    new Ingredient("Sugar", 100),
+    new Ingredient("Milk", 200),
+    new Ingredient("Eggs", 3),
+    new Ingredient("Butter", 50),
+    new Ingredient("Baking Powder", 1),
+    new Ingredient("Salt", 1),
+  ];
 
 }
